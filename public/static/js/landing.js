ccc5b37794c31eb64f8604f3e07ace1e8e2e68c9
Leo Sanchez <sanchez.leo88@gmail.com>
2024-05-21 19:57:57 -0300

    Background change, commented out sliding banner elements (WIP)

--- public/static/js/landing.js
+++ public/static/js/landing.js
@@ -1,1 +1,20 @@
+// Sliding banner WIP
+// Unexpected behaviour of scrollIntoView
+// Must change for transitions/transformations
 
+const slides = document.getElementsByClassName("slide");
+
+let slideShown = 0;
+
+function cycleSlides() {
+  console.log("before", slideShown);
+  slides[slideShown].scrollIntoView();
+
+  slideShown++;
+
+  if (slideShown > slides.length - 1) {
+    slideShown = 0;
+  }
+}
+
+// const slidesInterval = setInterval(cycleSlides, 5000);
